9229962d4126a35ff3641afcaf8dba78d75e8a5f
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-12-11 17:59:17 +0100

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,8 @@ $(function() {
 	}
 
 	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
