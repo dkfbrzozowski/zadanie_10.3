7fc25015ae97ea2415b4d004cb4cad81199b58b2
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-12-13 00:46:53 +0100

    Set interval

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,61 @@
 $(function() {
+	var carouselList = $(".photo"),
+		carouselMenu = $(".carousel-indicators"),
+		carouselLeft = $(".fa-angle-left"),
+		carouselRight = $(".fa-angle-right");
+		
+
+	carouselList.find("li").each(function() {
+		carouselMenu.append("<li></li>");
+	});
+	
+	var menuDots = carouselMenu.find("li");
+	menuDots.first().addClass("active");
+
+	menuDots.click(function() {
+		target = $(this).index();
+		moveSlider(target);
+	});
+
+	function moveSlider(target) {
+		carouselList.stop().animate({"left": -640 * target});
+		menuDots.removeClass("active").eq(target).addClass("active");
+	};
+
+	//============
+	//set interval
+
+	slideInterval = setInterval(changeSlide, 3000);
+
+	function changeSlide() {
+		carouselList.animate({"margin-left": -640}, 500, moveFirstSlide);
+	};
+	
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first"),
+			lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+
+	//======================
+	//set navigation - right (po kliknięciu resetujemy interval następnie 
+	//uruchamiamy przewinięcie na następny slajd i ponownie ustawiamy interval)
+
+	carouselRight.click(function() {
+		clearInterval(slideInterval);
+		changeSlide();
+		setInterval(changeSlide, 3000);
+	});
+
+
+/*	
 	var carouselList = $("#carousel ul"),
 		setTimeInterval,
 		slideWidth = 640,
-		animationTime = 1000,
-		firstItem = carouselList.find("li:first"),
-		lastItem = carouselList.find("li:last");
+		animationTime = 1000;
 
-	function setSlideInterval() {
-		setTimeInterval = setInterval(changeSlide, 3000);
-	}
-	setSlideInterval();
-
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-slideWidth}, animationTime, moveFirstSlide);
-	}
-
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first"),
-		lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	}
-/*
+	
 	$('#right').click(function() {
 		clearInterval(setTimeInterval);
 		changeSlide();
